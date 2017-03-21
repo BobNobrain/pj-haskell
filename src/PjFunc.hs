a43dd408476e656576a8b977cc5e4a83c71af09a
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-03-21 06:55:54 +0300

    Implemented add and rm

--- src/PjFunc.hs
+++ src/PjFunc.hs
@@ -7,7 +7,7 @@ module PjFunc
 
 import System.Path.NameManip (guess_dotdot, absolute_path)
 import System.FilePath (addTrailingPathSeparator, normalise)
-import System.Directory (getHomeDirectory, doesFileExist)
+import System.Directory (getHomeDirectory, doesFileExist, copyFile, removeFile)
 import System.IO hiding (readFile)
 import Data.Maybe (fromJust)
 import Data.List (isPrefixOf)
@@ -25,16 +25,19 @@ absolutize aPath
 
 
 filepath = "~/.pj"
+tempFilepath = "~/.pj.temp"
 
-openConfigReadWrite :: IO Handle
-openConfigReadWrite = do
+openConfig :: IOMode -> IO Handle
+openConfig m = do
     path <- absolutize filepath
-    openFile path ReadWriteMode
+    openFile path m
 
-openConfigWrite :: IO Handle
-openConfigWrite = do
-    path <- absolutize filepath
-    openFile path AppendMode
+data Modification
+    = Write String
+    | Skip
+    | WriteUnmodified String
+
+type Modifier = String -> Modification
 
 
 -- reads a single entry from pj file and prints it
@@ -49,12 +52,26 @@ pjget name = do
 pjadd :: String -> String -> IO ()
 pjadd name path =
     if validateName name then do
-        fileH <- openConfigWrite
+        -- check if we are modifying existing entry or adding new one
+        let
+            modifier :: String -> String -> Modifier
+            modifier name path str
+                | head (words str) == name = Write (name ++ " " ++ path)
+                | otherwise = WriteUnmodified str
+        modifiedCount <- modifyConfig $ modifier name path
         aPath <- absolutize path
-        hPutStrLn fileH (name ++ " " ++ aPath)
-        hClose fileH
-        putStrLn $ "Added '" ++ name ++ "' project at path '" ++ path ++ "'"
-        putStrLn $ "(absolute is '" ++ aPath ++ "')"
+
+        if modifiedCount == 0 then do
+            -- there were no previous entry about $name
+            fileH <- openConfig AppendMode
+            hPutStrLn fileH (name ++ " " ++ aPath)
+            hClose fileH
+            putStrLn $ "Added '" ++ name ++ "' project at path '" ++ path ++ "'"
+            putStrLn $ "(absolute is '" ++ aPath ++ "')"
+        else do
+            -- there was an entry, and it was modified
+            putStrLn $ "Changed '" ++ name ++ "' project to path '" ++ path ++ "'"
+            putStrLn $ "(absolute is '" ++ aPath ++ "')"
     else do
         putStrLn $ "Invalid project name '" ++ name ++ "'"
         putStrLn "Project name should not contain whitespace"
@@ -62,11 +79,17 @@ pjadd name path =
 
 -- removes one or more entries from pj file
 pjrm :: [String] -> IO ()
-pjrm names = putStrLn $ "pj-rm " ++ (unwords names)
+pjrm names = (modifyConfig $ modifier names) >> return ()
+    where
+        modifier :: [String] -> Modifier
+        modifier [] str = WriteUnmodified str
+        modifier names str = if name `elem` names then Skip else WriteUnmodified str
+            where
+                name = head $ words str
 
 -- lists the pj file entries
 pjlist :: IO ()
-pjlist = openConfigReadWrite >>= loopFile' >>= showCount
+pjlist = (openConfig ReadWriteMode) >>= loopFile' >>= showCount
     where
         loopFile' :: Handle -> IO Int
         loopFile' h = loopFile h putStrLn
@@ -87,7 +110,6 @@ loopFile = loop 0
                 str <- hGetLine fileH
                 callback str
                 loop (n + 1) fileH callback
-        
 
 
 validateName :: String -> Bool
@@ -98,3 +120,41 @@ validateName str = validateName' str
         validateName' (' ':xs) = False
         validateName' (x:xs) = validateName' xs
         validateName' "" = True
+
+
+modifyConfig :: Modifier -> IO Int
+modifyConfig modifier = do
+    -- cp ~/.pj ~/.pj.temp
+    pjTemp <- absolutize tempFilepath
+    pjFile <- absolutize filepath
+    copyFile pjFile pjTemp
+    -- preparing handles
+    tempFileH <- openFile pjTemp ReadMode
+    pjFileH <- openFile pjFile WriteMode
+    -- main loop here: line-by-line reading and writing
+    n <- loop 0 pjFileH tempFileH modifier
+    -- resource freeing
+    hClose tempFileH
+    hClose pjFileH
+    -- cleanup
+    removeFile pjTemp
+    return n
+        where
+            loop :: Int -> Handle -> Handle -> Modifier -> IO Int
+            loop modifiedCount fileR fileW modifier = do
+                isEof <- hIsEOF fileR
+                if isEof then
+                    return modifiedCount
+                else do
+                    readStr <- hGetLine fileR
+                    n <- modify modifiedCount fileW $ modifier readStr
+                    loop n fileR fileW modifier
+                        where
+                            modify :: Int -> Handle -> Modification -> IO Int
+                            modify n fileW Skip = return (n + 1)
+                            modify n fileW (WriteUnmodified s) = do
+                                hPutStr fileW s
+                                return n
+                            modify n fileW (Write s) = do
+                                hPutStr fileW s
+                                return (n + 1)
