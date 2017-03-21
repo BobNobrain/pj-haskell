406ab9b028391e188d5e1bb05af3bf7628d4c1dc
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-03-22 02:04:24 +0300

    Added help messages

--- src/PjHelp.hs
+++ src/PjHelp.hs
@@ -4,7 +4,40 @@ module PjHelp
     ) where
 
 pjhelp :: String -> [String]
-pjhelp _ = ["Help not done yet", "Please, stand by"]
+
+pjhelp ""       =   [ "pj - filesystem path labeling program"
+                    , "Usage:"
+                    , "  pj <name>                         - navigates to a path labeled as <name>"
+                    , "  pj add <name> <path>              - adds a label <name> for <path>"
+                    , "  pj rm <name1> [, <name2> [, ...]] - removes labels with given names"
+                    , "  pj list                           - lists all existing labels"
+                    , "  pj --help                         - prints this message"
+                    , "Type pj <cmd> --help for help about commands"
+                    ]
+pjhelp "add"    =   [ "pj add - create a new filesystem path label or update existing"
+                    , "Usage:"
+                    , "  pj add <name> <path>"
+                    , "  - <name>: a unique label name (string, should not contain whitespace)"
+                    , "  - <path>: filesystem path to label with <name>"
+                    , "If a label with a given name already exists, its path will be changed."
+                    , "If there is already a label for <path>, another label will be added for it."
+                    , "Type pj --help for more common help message"
+                    ]
+pjhelp "rm"     =   [ "pj rm - remove a filesystem path label or labels"
+                    , "Usage:"
+                    , "  pj rm <name1> [, <name2> [, ...]]"
+                    , "Command removes all labels with names exact <name1>, <name2>, etc."
+                    , "If there are no labels with given names, nothing will happen."
+                    , "Type pj --help for more common help message"
+                    ]
+pjhelp "list"   =   [ "pj list - list all existing filesystem path labels"
+                    , "Usage:"
+                    , "  pj list"
+                    , "Prints all created labels and their count."
+                    , "Type pj --help for more common help message"
+                    ]
+
+pjhelp _ = ["Cannot show help for such query"]
 
 printHelp :: String -> IO ()
 printHelp cmd = mapM_ putStrLn $ pjhelp cmd
